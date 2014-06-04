104227b9931bfd89369366c6abce56cb1e0e86c4
Seth March <seth.d.march@gmail.com>
2014-06-04 17:41:33 -0500

    style for gameboard page

--- app/routes/games.js
+++ app/routes/games.js
@@ -1,11 +1,20 @@
 'use strict';
 
-var traceur = require('traceur');
-var Game = traceur.require(__dirname + '/../models/game.js');
+// var traceur = require('traceur');
+// var Game = traceur.require(__dirname + '/../models/game.js');
 
-exports.index = (req, res)=>{
-  Game.findOpenGames(games=>
-  {
-    res.render('games/index', {games: games, title: 'Play Game'});
-  });
+// exports.index = (req, res)=>{
+//   Game.findOpenGames(games=>
+//   {
+//     res.render('games/index', {games: games, title: 'Play Game'});
+//   });
+// };
+
+exports.index = (req, res, next)=>{
+  var games = [{_id:'538f7018ebcb5274ef6d4c8d'}];
+    res.render('games/index', {title: 'Game Index Page', games:games});
+};
+
+exports.play = (req, res, next)=>{
+  res.render('games/play', {title: 'Game ID Page'});
 };
